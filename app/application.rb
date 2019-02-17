c4da8dd96d60763f137eb12d8700b145054fca89
Marie Burns <mcburns2222@gmail.com>
2019-02-17 18:31:02 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -20,7 +20,7 @@ class Application
         resp.write "#{item}\n"
       end
     elsif req.path.match(/add/)
-      item = req.params["q"]
+      item = req.params["item"]
       if @@items.include?(item)
         @@cart << item
         resp.write "added #{item}"
